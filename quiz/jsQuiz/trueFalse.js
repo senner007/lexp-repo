9ec1937375c5d7240eb14c93440c684f32972c63
senner007 <nielshtg@hotmail.com>
2014-06-06 19:05:57 +0200

    trueFalse.js clean up and add deferred loading

--- quiz/jsQuiz/trueFalse.js
+++ quiz/jsQuiz/trueFalse.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+// deferred to be resolved when shapeshift has loaded
+var defShapeShiftReady = new $.Deferred();
+$.getScript("js/jquery.shapeshift.js", function(data, textStatus, jqxhr) {
+		defShapeShiftReady.resolve();
+});
+
 var initCalc = {
 
 	xmlData:	function () {
@@ -67,38 +73,13 @@ var initCalc = {
 			var lastPart = '-' + e.substr(rounded,e.length);
 			return [firstPart, lastPart, i];
 	},
-	/* splitArr:	function (arr) {
-	
-			var count = 1;
-			while ( count > 0 ) {
-				count = 0;
-				var stext;
-				$.each(arr, function (i,e) {
-				
-					//totalChar += e.length;
-					if ( e.length > 470) {
-						count = 1;
-						stext =  initCalc.splitArrHelper(e,i);
-												
-					}	
-				
-				})
-				if (count == 1) {
-				// insert split text, remove from array then run above again
-				arr.splice(stext[2],1,stext[0],stext[1]);  
-
-				}
-
-			}
-			return arr;
-	
-	}, */
 	countLi:	function (arr) {
 			var newTotal =  Math.round((arr.length / 3.5));	
 			return newTotal;
 	
 	},
 };
+
 var app = {};
 app.textfile = initCalc.textfile ( initCalc.xmlData(), initCalc.thisQuiz() );
 app.quoteText = $( initCalc.xmlData() ).find( initCalc.thisQuiz() ).find('quoteText').text();
@@ -106,17 +87,10 @@ app.array = initCalc.stringArr ( app.textfile );
 app.cleanArray = initCalc.removeEmpty( app.array );
 app.numberOfPages = initCalc.countLi( app.cleanArray );
 app.trueArray = initCalc.xmlTrue(initCalc.xmlData(), initCalc.thisQuiz());
-
-
 app.falseArray = initCalc.xmlFalse(initCalc.xmlData(), initCalc.thisQuiz());
 
-
-
-
- var initActions = {
- 
-	
-	
+var initActions = {
+ 	
 	createLis:	function (countedLis) {
 	
 			for (var i=1;i <((countedLis * 2) + 2);i = i +2) {   			// assign id's to containers
@@ -146,9 +120,7 @@ app.falseArray = initCalc.xmlFalse(initCalc.xmlData(), initCalc.thisQuiz());
 				if (cHeight > 420 ) { 
 					var cutOff = $(el).children().last().text().length - ((cHeight - 420) * 3)
 					var tsplit = initCalc.splitArrHelper( $(el).children().last().text(), i, cutOff )
-					
-					
-					
+															
 					$(el).children().last().remove();
 					$("<div class='object'></div>").text(tsplit[0]).appendTo(el);
 				
@@ -160,8 +132,6 @@ app.falseArray = initCalc.xmlFalse(initCalc.xmlData(), initCalc.thisQuiz());
 	
 	},
 	placeQnA:	function (q,a) {
-	
-		
 			
 				var myHtmlq = '';
 				var myHtmla = '';
@@ -175,9 +145,7 @@ app.falseArray = initCalc.xmlFalse(initCalc.xmlData(), initCalc.thisQuiz());
 				}		
 				$(myHtmlq).appendTo( $('#frame').find('.lis').last().find('.container').first() );
 				$(myHtmla).appendTo( $('#frame').find('.lis').last().find('.container').last() );				
-		
-		
-	
+					
 	}
 
  }
@@ -186,17 +154,11 @@ initActions.createLis ( app.numberOfPages );
 initActions.placeObjects ( app.cleanArray );
 
 // remove empty pages
- $('#container').find(".container:empty").remove();
+$('#container').find(".container:empty").remove();
 $('#frame').find(".conLi:empty").parent().remove(); 
 // insert a new page for true and False 
-$('.slidee').append("<li class='lis'><div class='conLi'><div class='container' id='trueFalse'></div><div class='container' id='trueFalse2'></div></div></li>");
-
- initActions.placeQnA ( app.trueArray , app.falseArray );
-$('#frame').find('.lis').last().find('.container').first().addClass('tr');
-$('#frame').find('.lis').last().find('.container').last().addClass('fls');
- 
-//remove empty pages 
-
+$('.slidee').append("<li class='lis'><div class='conLi'><div class='container tr' id='trueFalse'></div><div class='container fls' id='trueFalse2'></div></div></li>");
+initActions.placeQnA ( app.trueArray , app.falseArray );
 
 
  $.fn.shuffle2 = function() {
@@ -229,71 +191,66 @@ if (docWidth > 1100) {
 	$('<button id="nextButton"></button><button id="prevButton"></button>').appendTo('#container');
 }
 
-    var frame = $('#frame');
-    var items = frame.find('ul > li');
-    var sly = new Sly(frame, {
-        horizontal: 1,
-        itemNav: 'forceCentered',
-        scrollBy: 1,
-        speed: 400,
-		startAt: 0,
-		activateMiddle: 1,
-		touchDragging: 0,
-		releaseSwing:  1,
-		elasticBounds: 1,
-		easing: 'easeInOutCirc', 
-		dynamicHandle: 1,
-		clickBar: 1,
-		dragSource:	$('#frame'),
-		//cycleBy:       'pages', // Enable automatic cycling by 'items' or 'pages'.
-		//cycleInterval: 1600, // Delay between cycles in milliseconds.
-		//pauseOnHover:  0,    // Pause cycling when mouse hovers over the FRAME.
-	//	startPaused:   1, 
-		pagesBar: $('.pages'),
-		activatePageOn: 'click',
-		keyboardNavBy: 'pages',
-		},
-		{
-		moveEnd: function () {
-			if (this.pos.end == this.pos.dest ) {
-				if ( $('.active').css('backgroundColor') != 'rgb(72, 72, 72)' ) { 
-		
-				$('#trueFalseBox').css('color', 'rgb(72, 72, 72)') };
-				$('#trueFalseBox').show().transition({opacity: 1}, 500);
-			}
-			else {
-				if ( $('#trueFalseBox').css('opacity') != 0 ) {
-				$('#trueFalseBox').transition({opacity: 0}, 200, function () {
-					$(this).hide();
-				});
-				}
-			}
-			
-		
-		
-		
+var frame = $('#frame');
+var items = frame.find('ul > li');
+var sly = new Sly(frame, {
+	horizontal: 1,
+	itemNav: 'forceCentered',
+	scrollBy: 1,
+	speed: 400,
+	startAt: 0,
+	activateMiddle: 1,
+	touchDragging: 0,
+	releaseSwing:  1,
+	elasticBounds: 1,
+	easing: 'easeInOutCirc', 
+	dynamicHandle: 1,
+	clickBar: 1,
+	dragSource:	$('#frame'),
+	//cycleBy:       'pages', // Enable automatic cycling by 'items' or 'pages'.
+	//cycleInterval: 1600, // Delay between cycles in milliseconds.
+	//pauseOnHover:  0,    // Pause cycling when mouse hovers over the FRAME.
+//	startPaused:   1, 
+	pagesBar: $('.pages'),
+	activatePageOn: 'click',
+	keyboardNavBy: 'pages',
+	},
+	{
+	moveEnd: function () {
+		var $tFBox = $('#trueFalseBox');
+		if (this.pos.end == this.pos.dest ) {			
+			$tFBox.css('color', 'rgb(72, 72, 72)').show().transition({opacity: 1}, 500);
 		}
-			
-    }).init(); 
+		else {
+			if ( $tFBox.css('opacity') != 0 ) {
+			$tFBox.transition({opacity: 0}, 200, function () {
+				$(this).hide();
+			});
+			}
+		}
+
+	}
+		
+}).init(); 
 	
 	
+$.when(defShapeShiftReady).done(function () { // wait for shapeshift script to load to ensure ui preload dependency	
+
+	$("#container").find('.lis').last().find(".container").shapeshift({
+		paddingY: 0,
+		paddingX: 0,
+		gutterY: 6,
+		gutterX: 0,
+		minHeight: 499,
+		animateSpeed: 120
 	
-	  $("#container").find('.lis').last().find(".container").shapeshift({
-	paddingY: 0,
-	paddingX: 0,
-	gutterY: 6,
-	gutterX: 0,
-	minHeight: 499,
-	animateSpeed: 120
+	});
 	
-  });
-  
+});  
  
 $('#quoteText').text( app.quoteText ); 
 
-
-
-  sly.on('change', function () {	// listen for when sly is at biginning and end and add class to prev/next buttons
+sly.on('change', function () {	// listen for when sly is at biginning and end and add class to prev/next buttons
         // Check whether Sly is at the start
         $('#prevButton')[this.pos.start === this.pos.dest ? 'addClass' : 'removeClass']('ButtonDisabled');
         // Check whether Sly is at the end
@@ -340,22 +297,18 @@ preload([
     'css/cssImg/orangeCheck2.png',
     'css/cssImg/cancel.png'
 ]);
- 
-			
 
 
 var actions = {
 
 	checkFunction: function () {	
-		
-	
+			
 		$('#shuffleButton').css('text-decoration','line-through').off();
 		$('#checkButton').css('text-decoration','line-through').off();
 		
-		 $('.object.tr,.object.fls').draggable('disable');
-		 sly.toEnd();
-		 sly.set({ speed: 1000, easing: 'easeInOutCubic'});
-
+		$('.object.tr,.object.fls').draggable('disable');
+		sly.toEnd();
+		sly.set({ speed: 1000, easing: 'easeInOutCubic'});
 
 		var easingOut = 'ease';
 		if (!$.support.transition) {
@@ -364,17 +317,13 @@ var actions = {
 		}
 		var easingIn = 'easeOutQuad';
 		var correctNumber = 0;
-
 		
 		$('.object.tr,.object.fls').each(function (index2, e) {
 			
-		
-		
 			var $this = $(this);
 			var $thisText = $(this).text();
 			
 			if ( ($this.hasClass('tr') && $this.parent().hasClass('tr'))  || ($this.hasClass('fls') && $this.parent().hasClass('fls')) ) {
-	/* 		console.log('yes') */
 			
 				$this.append("<img class='imgCheck' src='css/cssImg/check2.png'/>");
 				
@@ -388,70 +337,67 @@ var actions = {
 					var options = {
 							marginTop: 10,
 							color: '#006400'
-						}
+					}
 				}
 					
-					$this.delay(index2 * 250)
+				$this.delay(index2 * 250)
 					.transition(options,130, easingOut)
 					.animate({
 						marginTop: 0
-				}, 100, easingIn,function () {
-				if (highLightThis == true) { 
-					
-					$this.highlight($thisText, { className: 'greenColor'})
-					
-				}
+					}, 100, easingIn,function () {
+						if (highLightThis == true) { 
+							
+							$this.highlight($thisText, { className: 'greenColor'})
+							
+						}
 			 
-				var thisPos =  $this.find('img').position()
+						var thisPos =  $this.find('img').position()
 				
-				if ( (thisPos.left > 468 && thisPos.left < 480) || (thisPos.left > 820 && thisPos.left < 830) ) {
-					$this.find('img').css({ x: -26, y: 5 })
-				}
-					$this.find('img').transition({opacity: 1});
-					correctNumber++;
+						if ( (thisPos.left > 468 && thisPos.left < 480) || (thisPos.left > 820 && thisPos.left < 830) ) {
+							$this.find('img').css({ x: -26, y: 5 })
+						}
+							$this.find('img').transition({opacity: 1});
+							correctNumber++;
 		
-				});	
+					});	
 			}
 			else {
-			
-			/* console.log('no') */
-					$this.append("<img class='imgCheck' src='css/cssImg/cancel.png'/>");
-					if ( $this.parents('.slidee li').css('backgroundColor') == 'rgb(72, 72, 72)') {
-						var highLightThis = true;
-						var options = {
-							marginTop: 10
-						}
+
+				$this.append("<img class='imgCheck' src='css/cssImg/cancel.png'/>");
+				if ( $this.parents('.slidee li').css('backgroundColor') == 'rgb(72, 72, 72)') {
+					var highLightThis = true;
+					var options = {
+						marginTop: 10
 					}
-					else {
-						var options = {
-							marginTop: 10,
-							color: '#A80000'
-						}
+				}
+				else {
+					var options = {
+						marginTop: 10,
+						color: '#A80000'
 					}
-			
-					$this.delay(index2 * 250)
+				}
+		
+				$this.delay(index2 * 250)
 					.transition(options,130,easingOut)
 					.animate({
 						marginTop: 0
 					}, 100,easingIn,function () {
-					if (highLightThis == true) { 
-						$thisText = $.trim($thisText);
-						$this.highlight($thisText, { className: 'redColor'}) 
-					} 
-					
-					
-					var thisPos =  $(this).find('img').position()
-					
-					$this.find('img').css({ y: +2,marginLeft: '4px' })
-						if ( (thisPos.left > 468 && thisPos.left < 480) || (thisPos.left > 820 && thisPos.left < 830) ) {
-						
-						$this.find('img').css({ x: - 15 })
-					} 
-					$this.find('img').transition({opacity: 1});
-					
-					})
+						if (highLightThis == true) { 
+							$thisText = $.trim($thisText);
+							$this.highlight($thisText, { className: 'redColor'}) 
+						} 
 			
+						var thisPos =  $(this).find('img').position()
+						
+						$this.find('img').css({ y: +2,marginLeft: '4px' })
+							if ( (thisPos.left > 468 && thisPos.left < 480) || (thisPos.left > 820 && thisPos.left < 830) ) {
+							
+							$this.find('img').css({ x: - 15 })
+						} 
+						$this.find('img').transition({opacity: 1});
 				
+					})
+					
 			}
 		
 			
@@ -461,65 +407,31 @@ var actions = {
 					
 			$object.last().append("<div id='endText'></div>");
 			
-		/* 	if (correctNumber != myArr.length) { 
-				$object.find('#endText').text('Hit shuffle and try again')
-			}
-			if ((correctNumber + 5) >= myArr.length) { 
-				$object.find('#endText').text('Almost!') 
-			}
-			if (correctNumber == myArr.length) { 
-				$object.find('#endText').text('Well Done!') 
-			} 
-			if ($object.last().parents('.slidee li').css('backgroundColor') != 'rgb(72, 72, 72)') { 	$object.find('#endText').css('color','black'); 
-			} */
-			
 			$object.find('#endText').transition({opacity: 1});
 			setTimeout(function(){						
 				sly.set({ speed: 400, easing: 'easeInOutCirc'});
 			},1300);						
 		
-
 		});
 	
 	},	
 	shuffleFunction: function () { 
-	/* 	console.log('shuffle clicked');  */
-
+		var $tFobjects = $('#frame').find('.object.tr,.object.fls');
+		
 		$('#checkButton').css('text-decoration','none').off().on('tapclick', actions.checkFunction);
-	  
-		 $('#frame').find('.lis').last().find('.container').find('div').each(function () {
-		/*  console.log('fdfddfd'); */
-		$this = $(this)
-			if ( $this.parents('.slidee li').css('backgroundColor') != 'rgb(72, 72, 72)'){
-			
-			$this.css('color','rgb(68, 68, 68)');
-			}
-			$this.unhighlight();
-			
+		$tFobjects.each(function () {
+			$(this).css('color','rgb(80, 80, 80)').unhighlight();			
 		})
 		$('img').remove();
 		$('#endText').remove();
-		 $('#frame').find('.lis').last().find('.container').find('div').find('span').contents().unwrap();
-			
-			
-			$('.object.tr,.object.fls').shuffle2();
-	
+		$tFobjects.find('span').contents().unwrap();
+		$tFobjects.shuffle2();
+		$('.object.tr,.object.fls').trigger("ss-event-arrange").trigger("ss-event-dragreset");
 		
-		  $('.object.tr,.object.fls').trigger("ss-event-arrange");
-		$('.object.tr,.object.fls').trigger("ss-event-dragreset");
-		// sly.destroy().init(); 
-	
-			
 	}
 }
 
-
-
-
- $('#checkButton').on('tapclick',actions.checkFunction);
- $('#shuffleButton').on('tapclick',actions.shuffleFunction); 
-
-
-
+$('#checkButton').on('tapclick',actions.checkFunction);
+$('#shuffleButton').on('tapclick',actions.shuffleFunction); 
 
 });
