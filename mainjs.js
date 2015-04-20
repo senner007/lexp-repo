2f1a5203aac2afa071e3970997ac89cd45d67e3c
senner007 <nielshtg@hotmail.com>
2015-04-20 20:01:44 +0200

    gapfill responsive scrap

--- mainjs.js
+++ mainjs.js
@@ -354,11 +354,7 @@ $('#containerIsotope').on('tapclick','div',function(ev) {
 });	
 
 function LoadIframe($div) {
-	
-	window.windowHeight = window.innerHeight  //accounts for ipad navigation bars
-	var winHeight = window.innerHeight;
-	if (winHeight <  705) {winHeight = 620}					//  will only display full screen mode on tablet if above a certain value. Means that the user can 
-															//	choose between 'compressed' design or fullscreen when the browser navn is hidden
+
 
 	this.$div = $div
 
@@ -403,7 +399,7 @@ function LoadIframe($div) {
 	}
 	if (this.$div.hasClass('gapFill') ){					// gapfill
 		this.qhref = 'quiz/gapFill.html';
-		this.quizHeight = Math.min(Math.max((winHeight), 600), 800);				// will set the height to min and max value
+		this.quizHeight = 640;				// will set the height to min and max value
 	}
 	if (this.$div.hasClass('transition') ){			//transition
 		this.qhref = 'quiz/transition.html';
